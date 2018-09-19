a716f9d9b23c2e5a72c0b08c992bab4b7b361890
Jason To <jasto@ucdavis.edu>
2018-09-19 22:25:15 +0000

    automatically backed up by learn

---
+++ lib/current_player.rb
@@ -0,0 +1,16 @@
+def turn_count(board)
+  turns_played = 0
+  while turns_played < 3
+    turns_played += 1
+  end
+  return turns_played
+end
+
+def current_player(board)
+  num = turn_count(board)
+  if num % 2 == 0
+    return "X"
+  else 
+    return "O"
+  end 
+end 
